a5750b28617003b87afb25ef82b321dcce871be8
Russ Gunther <165608775+russtacean@users.noreply.github.com>
2026-04-20 19:33:25 -0400

    Use petal as our component library

--- config/config.exs
+++ config/config.exs
@@ -102,6 +102,11 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configure Petal Components to use our error translation function
+config :petal_components,
+       :error_translator_function,
+       {DoggoWeb.CoreComponents, :translate_error}
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
